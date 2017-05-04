04d06c87d10ad91c988f0bdf2bb9e6139aaea0af
edouardbozon <bozon.edouard@live.fr>
2017-05-04 14:07:19 +0200

    Pipe directly ffmpeg stream in response

--- server.js
+++ server.js
@@ -11,35 +11,43 @@ const port = 3000
 const url = 'https://www.youtube.com/watch?v=kK4D5R-0mY0'
 
 app.get('/', (request, response) => {
-  // SET TITLE
-  ytdl.getInfo(url, function(err, info) {
-    if (err) throw err
-    response.setHeader('Content-disposition', 'attachment; filename=' + info.title + '.mp3')
-    response.setHeader('Content-type', 'audio/mpeg')
+  
+  const SOURCE_FORMAT = 'mp4';
+  const DESTINATION_FORMAT = 'mp3';
+  const CODEC = 'libmp3lame';
+  const source = __dirname + '/source.' + SOURCE_FORMAT
+  const audio = __dirname + '/source.' + DESTINATION_FORMAT;
+
+  let sourceTitle = null;
+  let totalLength = 0;
+  const stream = ytdl(url, { 
+    filter: (format) => format.container === SOURCE_FORMAT && !format.encoding
   })
-  // GET VIDEO AND TRANSFORM IT TO AUDIO
-  const source = ytdl(url, { filter: (f) => {
-    return f.container === 'mp4' && !f.encoding; }
+  .on('info', (info, format) => {
+    sourceTitle = info.title;
   })
-  const sound = ffmpeg({ source: source })
-  sound.withAudioCodec('libmp3lame')
-  sound.toFormat('mp3')
-  sound.output(response)
-  sound.run()
-  sound.on('end', () => {
-    console.log('DOWNLOAD FINISHED\n')
-  })
-  sound.on('error', (err) => {
-    console.log('an error happened: ' + err.message)
-  })
-  sound.on('progress', (progress) => {
-    console.log('Processing: ' + progress.targetSize + ' KB converted')
-  })
-})
+  .on('progress', (chunk, total, totalLength) => {
+    totalLength = totalLength;
+  });
+
+  ffmpeg()
+    .input(stream)
+    .withAudioCodec(CODEC)
+    .toFormat(DESTINATION_FORMAT)
+    .on('end', () => {
+      console.log('DOWNLOAD FINISHED\n')
+    })
+    .on('error', (err) => {
+      console.log('an error happened: ' + err.message)
+    })
+    .on('progress', (progress) => {
+      console.log('Processing: ' + progress.targetSize + ' KB converted')
+    })
+    .pipe(response);
+});
 
 app.listen(port, hostname, (error) => {
-  if (error) {
-    return console.log('something bad happened', error)
-  }
+  if (error) return console.error(error)
+
   console.log(`Server running at http://${hostname}:${port}/`)
 })
