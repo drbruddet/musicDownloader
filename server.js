bc339a63a1436bd8fa88425ef7eca65be524785b
Guillaume Kolly <guillaume.kolly@gmail.com>
2017-05-05 11:12:41 +0200

    getTitle and set it to header

--- server.js
+++ server.js
@@ -10,58 +10,51 @@ const port = 3000
 
 const url = 'https://www.youtube.com/watch?v=kK4D5R-0mY0'
 
+getMediaTitle = (url, getTitle) => {
+  ytdl.getInfo(url, function(err, info) {
+    getTitle(info.title)
+  })
+}
+
 app.get('/', (request, response) => {
-  
+
   const SOURCE_FORMAT = 'mp4';
   const DESTINATION_FORMAT = 'mp3';
   const CODEC = 'libmp3lame';
-  const source = __dirname + '/source.' + SOURCE_FORMAT
-  const audio = __dirname + '/source.' + DESTINATION_FORMAT;
 
-  let sourceTitle = null;
   let totalLength = 0;
-  const stream = ytdl(url, { 
+  const stream = ytdl(url, {
     filter: (format) => format.container === SOURCE_FORMAT && !format.encoding
   })
-  .on('info', (info, format) => {
-    sourceTitle = info.title;
-  })
-  .on('progress', (chunk, total, totalLength) => {
-    totalLength = totalLength;
-  });
 
   try {
-    const fileName = sourceTitle + '.' + SOURCE_FORMAT;
-
-    response.setHeader('Content-Disposition', 'attachment; filename=' + fileName);
-    response.setHeader('Content-Length', totalLength);
-    response.setHeader('Content-type', 'audio/mpeg')
+    getMediaTitle(url, function(title) {
+      response.setHeader('Content-Disposition', 'attachment; filename=' + title + '.' + DESTINATION_FORMAT);
+      response.setHeader('Content-type', 'audio/mpeg')
+    })
 
     ffmpeg()
     .input(stream)
     .withAudioCodec(CODEC)
     .toFormat(DESTINATION_FORMAT)
-    .on('end', () => {
-      console.log('DOWNLOAD FINISHED\n')
-    })
     .on('error', (err) => {
       console.log('an error happened: ' + err.message)
     })
     .on('progress', (progress) => {
       console.log('Processing: ' + progress.targetSize + ' KB converted')
     })
+    .on('end', () => {
+      console.log('DOWNLOAD FINISHED\n')
+    })
     .pipe(response);
-    
+
   } catch (error) {
     res.end(error);
     res.status(500);
   }
-
-  
 });
 
 app.listen(port, hostname, (error) => {
   if (error) return console.error(error)
-
   console.log(`Server running at http://${hostname}:${port}/`)
 })
