603974878c5e7fc4396c357bdff4bab85e9bd91a
Guillaume Kolly <guillaume.kolly@gmail.com>
2017-05-10 11:47:44 +0200

    try to Promise the ytdl video

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const path    = require('path')
 const express = require('express')
 const fs      = require('fs')
-const ytdl    = require('ytdl-core')
 const chalk   = require('chalk')
 const logger  = require('./utils/logger.constant')
 const videoConverter = require('./utils/videoConverter')
+const getVideo = require('./utils/getVideo')
 var url = require("url")
 
 const app = express()
@@ -27,27 +27,25 @@ app.get('/', (request, response) => {
         // LOGGER MARCHE PAS, Undefined
         console.log(logger.startDl("TOTO"));
 
-        ytdl(target, {
-            filter: (format) => format.container === SOURCE_FORMAT && !format.encoding
-        })
-        .on('info', (info, format) => title = info.title)
-        .on('progress', (chunkLength, totalDownloaded, totalLength) => {
-            if (totalDownloaded === totalLength) console.log(logger.endDl);
-        })
-        .pipe(fs.createWriteStream(SOURCE_FILE))
-        .on('finish', () => {
-            response.writeHead(200, {
-                'Content-Type': `audio/mpeg`,
-                'Content-Disposition': 'attachment; filename=' + title + '.' + DESTINATION_FORMAT,
-                'Access-Control-Allow-Origin': '*',
-            })
-            videoConverter.convertToMp3(SOURCE_FILE).then((converted) => {
-                converted.pipe(response, {end: true})
-                converted.on('finish', () => {
-                    console.log(chalk.green('Finished to emit stream to the client'))
-                    fs.unlink(sourceFile, () => console.log(chalk.green('Deleted temp file ') + chalk.bold(sourceFile)))
+        getVideo.youtube(target).then((video) => {
+            video.pipe(fs.createWriteStream(SOURCE_FILE))
+            video.on('finish', () => {
+                response.writeHead(200, {
+                    'Content-Type': `audio/mpeg`,
+                    'Content-Disposition': 'attachment; filename=' + title + '.' + DESTINATION_FORMAT,
+                    'Access-Control-Allow-Origin': '*',
+                })
+                videoConverter.convertToMp3(SOURCE_FILE).then((converted) => {
+                    converted.pipe(response, {end: true})
+                    converted.on('finish', () => {
+                        console.log(chalk.green('Finished to emit stream to the client'))
+                        fs.unlink(sourceFile, () => console.log(chalk.green('Deleted temp file ') + chalk.bold(sourceFile)))
+                    })
                 })
             })
+        }).catch((err) => {
+            console.error("ERROR")
+            console.dir(err)
         })
     } catch (error) {
         response.status(500).send(error);
